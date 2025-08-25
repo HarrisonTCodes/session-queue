6cd7fff54a2228d59c14d398b40ef26c2032e337
HarrisonTCodes <htillbackup@gmail.com>
2025-08-25 16:31:24 +0100

    feat: added JWT creation functionality

--- internal/routes/handler.go
+++ internal/routes/handler.go
@@ -3,17 +3,34 @@ package routes
 import (
 	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/HarrisonTCodes/session-queue/internal/redisclient"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func HandleStatus(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world")
 }
 
+var hmacSecret = []byte("secret")
+
 func HandleJoin(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
 	position, _ := redisclient.Rdb.Incr(ctx, "queue:current-position").Result()
-	fmt.Fprintf(w, "%s", fmt.Sprintf("%d", position))
+
+	claims := jwt.MapClaims{
+		"sub": position,
+		"iat": time.Now().Unix(),
+		"exp": time.Now().Add(time.Hour * 3).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(hmacSecret)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Write([]byte(`{"token":"` + tokenString + `"}`))
 }
