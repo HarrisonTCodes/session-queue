bb43636db34cfacdf9e62442d0c2cc91757df30c
HarrisonTCodes <htillbackup@gmail.com>
2025-08-25 20:31:21 +0100

    feat: added validate token function and hooked up status handler

--- internal/jwt/token.go
+++ internal/jwt/token.go
@@ -1,6 +1,8 @@
 package jwt
 
 import (
+	"errors"
+	"log"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -16,4 +18,25 @@ func CreateToken(position int64) (string, error) {
 	}
 	tkn := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return tkn.SignedString(hmacSecret)
+}
+
+func ValidateToken(tknString string) (int64, error) {
+	tkn, err := jwt.Parse(tknString, func(tkn *jwt.Token) (any, error) {
+		return hmacSecret, nil
+	}, jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Alg()}))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	claims, ok := tkn.Claims.(jwt.MapClaims)
+	if !ok {
+		return 0, errors.New("claims are not MapClaims")
+	}
+
+	sub, ok := claims["sub"].(float64)
+	if !ok {
+		return 0, errors.New("sub claim is not a number")
+	}
+
+	return int64(sub), nil
 }
--- internal/routes/handler.go
+++ internal/routes/handler.go
@@ -5,13 +5,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/HarrisonTCodes/session-queue/internal/jwt"
 	"github.com/HarrisonTCodes/session-queue/internal/redisclient"
 )
 
 func HandleStatus(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world")
+	tknHeader := r.Header.Get("Authorization")
+	if tknHeader == "" {
+		http.Error(w, "missing Authorization header", http.StatusBadRequest)
+		return
+	}
+
+	tknString := strings.TrimPrefix(tknHeader, "Bearer")
+	tknString = strings.TrimSpace(tknString)
+	fmt.Println(tknString)
+
+	pos, err := jwt.ValidateToken(tknString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Write([]byte(`{"position":"` + fmt.Sprintf("%v", pos) + `"}`))
 }
 
 func HandleJoin(w http.ResponseWriter, r *http.Request) {
