47f4044c602179a7002e9198018150f546ea3729
HarrisonTCodes <htillbackup@gmail.com>
2025-08-30 11:26:38 +0100

    refactor: added basic logging to main function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/HarrisonTCodes/session-queue/internal/config"
@@ -9,13 +10,17 @@ import (
 )
 
 func main() {
+	log.Println("Registering handlers")
 	mux := http.NewServeMux()
 	mux.HandleFunc("/status", routes.HandleStatus)
 	mux.HandleFunc("POST /join", routes.HandleJoin)
 
+	log.Println("Loading config")
 	config.Load()
 
+	log.Println("Connecting to Redis")
 	redisclient.Init(config.Cfg.RedisAddr)
 
+	log.Printf("Server running on port %s", config.Cfg.Port)
 	http.ListenAndServe(":"+config.Cfg.Port, mux)
 }
