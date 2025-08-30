961e203dcca08bc44f41729627f675f697983b3b
HarrisonTCodes <htillbackup@gmail.com>
2025-08-30 11:53:40 +0100

    feat: added window incrementing functionality to redis client

--- internal/redisclient/client.go
+++ internal/redisclient/client.go
@@ -3,11 +3,12 @@ package redisclient
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
 
-func Init(addr string) *redis.Client {
+func Init(addr string, windowSize int, windowSeconds int) *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
 		Addr: addr,
 	})
@@ -15,11 +16,28 @@ func Init(addr string) *redis.Client {
 	ctx := context.Background()
 	err := rdb.MSetNX(ctx,
 		"queue:current-position", 0,
-		"queue:current-max-allowed-position", 0,
+		"queue:current-max-allowed-position", windowSize,
 	).Err()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	go IncrWindow(rdb, ctx, windowSize, windowSize, windowSeconds)
+
 	return rdb
+}
+
+func IncrWindow(rdb *redis.Client, ctx context.Context, start int, size int, seconds int) {
+	ticker := time.NewTicker(time.Second * time.Duration(seconds))
+
+	defer ticker.Stop()
+
+	for range ticker.C {
+		log.Printf("Incrementing window from %d to %d", start, start+size)
+		start += size
+		err := rdb.IncrBy(ctx, "queue:current-max-allowed-position", int64(size)).Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
