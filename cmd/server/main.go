349c0e38e1f3bfa2ef0e7e6d254a6e0b6a437c71
HarrisonTCodes <htillbackup@gmail.com>
2025-08-31 10:40:36 +0100

    feat: added leadership election to window increment method

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -14,7 +14,7 @@ func main() {
 	cfg := config.Load()
 
 	log.Println("Connecting to Redis")
-	rdb := redisclient.Init(cfg.RedisAddr, cfg.WindowSize, cfg.WindowInterval)
+	rdb := redisclient.Init(cfg.InstanceId, cfg.RedisAddr, cfg.WindowSize, cfg.WindowInterval)
 
 	log.Println("Registering handlers")
 	mux := http.NewServeMux()
--- internal/redisclient/client.go
+++ internal/redisclient/client.go
@@ -9,7 +9,7 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-func Init(addr string, windowSize int, windowInterval int) *redis.Client {
+func Init(instanceId string, addr string, windowSize int, windowInterval int) *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
 		Addr: addr,
 	})
@@ -18,46 +18,84 @@ func Init(addr string, windowSize int, windowInterval int) *redis.Client {
 	err := rdb.MSetNX(ctx,
 		"queue:current-position", 0,
 		"queue:window-end", windowSize,
+		"queue:next-window-increment", windowInterval,
 	).Err()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	go IncrWindow(rdb, ctx, windowSize, windowInterval)
+	go IncrWindow(rdb, ctx, instanceId, windowSize, windowInterval)
 
 	return rdb
 }
 
-func IncrWindow(rdb *redis.Client, ctx context.Context, size int, interval int) {
-	checkDuration := time.Second * time.Duration(3)
-	intervalDuration := time.Second * time.Duration(interval)
-	nextIncr := time.Now().Add(intervalDuration)
+func IncrWindow(rdb *redis.Client, ctx context.Context, instanceId string, size int, interval int) {
+	checkDuration := time.Second * 3
 
 	for {
-		now := time.Now()
-		sleepDuration := max(nextIncr.Sub(now), 0)
-		time.Sleep(sleepDuration)
+		leaderId, err := rdb.Get(ctx, "queue:leader-id").Result()
+		if err != nil && err != redis.Nil {
+			log.Print("Redis error during leader retrieval:", err)
+			time.Sleep(checkDuration)
+			continue
+		}
 
-		vals, err := rdb.MGet(ctx, "queue:current-position", "queue:window-end").Result()
+		var isLeader bool
+		if err == redis.Nil {
+			log.Println("Electing self as leader")
+			setLeader, err := rdb.SetNX(ctx, "queue:leader-id", instanceId, time.Second*10).Result()
+			if err != nil {
+				log.Print("Redis error during leader election:", err)
+				time.Sleep(checkDuration)
+				continue
+			}
+			isLeader = setLeader
+		} else {
+			isLeader = leaderId == instanceId
+		}
+
+		if !isLeader {
+			log.Println("Self is not leader")
+			time.Sleep(checkDuration)
+			continue
+		}
+		log.Println("Self is leader")
+
+		vals, err := rdb.MGet(ctx, "queue:current-position", "queue:window-end", "queue:next-window-increment").Result()
 		if err != nil {
-			log.Println("Redis error:", err)
-			nextIncr = time.Now().Add(checkDuration)
+			log.Println("Redis error during retrieval of queue values:", err)
+			time.Sleep(checkDuration)
 			continue
 		}
 
 		currentPos, _ := strconv.Atoi(vals[0].(string))
-		end, _ := strconv.Atoi(vals[1].(string))
+		windowEnd, _ := strconv.Atoi(vals[1].(string))
+		nextUpdate, _ := strconv.ParseInt(vals[2].(string), 10, 64)
 
-		if currentPos > end {
-			log.Printf("Incrementing window from %d-%d to %d-%d", end-size, end, end, end+size)
-			err := rdb.IncrBy(ctx, "queue:window-end", int64(size)).Err()
-			if err != nil {
-				log.Fatal(err)
-			}
-			nextIncr = time.Now().Add(intervalDuration)
-		} else {
-			log.Printf("Skipping window increment as current position (%d) is inside current window (%d-%d)", currentPos, end-size, end)
-			nextIncr = time.Now().Add(checkDuration)
+		now := time.Now().Unix()
+		sleepDuration := time.Second * time.Duration(max(nextUpdate-now, 0))
+		if sleepDuration > 0 {
+			time.Sleep(sleepDuration)
+			continue
 		}
+
+		if currentPos > windowEnd {
+			log.Printf("Incrementing window from %d-%d to %d-%d", windowEnd-size, windowEnd, windowEnd, windowEnd+size)
+
+			pipe := rdb.TxPipeline()
+			pipe.IncrBy(ctx, "queue:window-end", int64(size))
+			newNextUpdate := now + int64(interval)
+			pipe.Set(ctx, "queue:next-window-increment", strconv.FormatInt(newNextUpdate, 10), 0)
+			_, err := pipe.Exec(ctx)
+			if err != nil {
+				log.Println("Redis error during incrementing window", err)
+				time.Sleep(checkDuration)
+				continue
+			}
+		} else {
+			log.Printf("Skipping window increment as current position (%d) is inside window (%d-%d)", currentPos, windowEnd-size, windowEnd)
+		}
+
+		time.Sleep(checkDuration)
 	}
 }
