d2912ec07f7737f50667f6ca3ebb06f126e51d2a
HarrisonTCodes <htillbackup@gmail.com>
2025-09-04 19:00:12 +0100

    chore: added leader already elected test

--- internal/redisclient/ops_test.go
+++ internal/redisclient/ops_test.go
@@ -22,3 +22,18 @@ func TestElectSelfAsLeader(t *testing.T) {
 		t.Fatalf("Expected leader to be id, got %v", leaderId)
 	}
 }
+
+func TestLeaderAlreadyElected(t *testing.T) {
+	s, _ := miniredis.Run()
+	defer s.Close()
+
+	ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{Addr: s.Addr()})
+	rdb.SetNX(ctx, "queue:leader-id", "other-id", 0)
+	ensureElectedLeader(rdb, ctx, "id", time.Second*10)
+
+	leaderId, _ := rdb.Get(ctx, "queue:leader-id").Result()
+	if leaderId != "other-id" {
+		t.Fatalf("Expected leader to be other-id, got %v", leaderId)
+	}
+}
