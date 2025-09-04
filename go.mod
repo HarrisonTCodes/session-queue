ac8bb04fd52227d98c831c52713ab346549c3963
HarrisonTCodes <htillbackup@gmail.com>
2025-09-04 18:59:47 +0100

    chore: added elect self as leader test

--- go.mod
+++ go.mod
@@ -2,10 +2,13 @@ module github.com/HarrisonTCodes/session-queue
 
 go 1.24.2
 
+require github.com/google/uuid v1.6.0
+
 require (
+	github.com/alicebob/miniredis/v2 v2.35.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/golang-jwt/jwt/v5 v5.3.0 // indirect
-	github.com/google/uuid v1.6.0
 	github.com/redis/go-redis/v9 v9.12.1 // indirect
+	github.com/yuin/gopher-lua v1.1.1 // indirect
 )
---
+++ internal/redisclient/ops_test.go
@@ -0,0 +1,24 @@
+package redisclient
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/alicebob/miniredis/v2"
+	"github.com/redis/go-redis/v9"
+)
+
+func TestElectSelfAsLeader(t *testing.T) {
+	s, _ := miniredis.Run()
+	defer s.Close()
+
+	ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{Addr: s.Addr()})
+	ensureElectedLeader(rdb, ctx, "id", time.Second*10)
+
+	leaderId, _ := rdb.Get(ctx, "queue:leader-id").Result()
+	if leaderId != "id" {
+		t.Fatalf("Expected leader to be id, got %v", leaderId)
+	}
+}
