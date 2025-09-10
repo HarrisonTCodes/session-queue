378d57e3e7444a529c13d4d47af7d14a92d05794
HarrisonTCodes <htillbackup@gmail.com>
2025-09-10 18:18:45 +0100

    chore: added get position status test

--- internal/queue/window_test.go
+++ internal/queue/window_test.go
@@ -28,3 +28,17 @@ func TestIncrementWindow(t *testing.T) {
 		t.Fatalf("Expected next-window-increment to have changed, got %v", nextWindowIncrement)
 	}
 }
+
+func TestGetPositionStatus(t *testing.T) {
+	if status := GetPositionStatus(1, 20, 10, 1); status != StatusExpired {
+		t.Fatalf("Expected status to be Expired, got %v", status)
+	}
+
+	if status := GetPositionStatus(15, 20, 10, 1); status != StatusActive {
+		t.Fatalf("Expected status to be Active, got %v", status)
+	}
+
+	if status := GetPositionStatus(30, 20, 10, 1); status != StatusWaiting {
+		t.Fatalf("Expected status to be Waiting, got %v", status)
+	}
+}
